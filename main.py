ae833515c08cbb662a239301b5468d265157cf5d
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2023-03-16 18:52:50 +0000

    minor progress

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import streamlit as st
-from datetime import date
+###Experimenting with other approacches
+# import streamlit as st
+# from datetime import date
+# #import phropet from fbprophet
+# from fbprophet import Prophet
+# from fbprophet.plot import plot_plotly
+# from plotly import graph_objs as go
 
-from fbprophet import prophet
-from fbprophet.plot import plot_plotly
-from plotly import graph_objs as go
+# START = "2015-01-01"
+# TODAY = date.today().strftime("%Y-%m-%d")
 
-START = "2015-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
+# st.title("Stock Predicition on the GSE")
 
-st.title("Stock Predicition on the GSE")
+# stocks = ("AADS","ABL")
+# selected_stocks = st.selectbox("select dataset for prediction", stocks)
 
-stocks = ("AADS","ABL")
-selected_stocks = st.selectbox("select dataset for prediction", stocks)
-
-n_years = st.slider("Years of prediction:", 1,2, 3)
-period = n_years*365
+# n_years = st.slider("Years of prediction:", 1,2, 3)
+# period = n_years*365
