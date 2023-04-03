cc5767cf5a55edd15e1911f877a22155eb80f6af
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2023-04-03 09:57:13 +0000

    prophet is live and working

---
+++ helper.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from plotly import graph_objs as go
+import streamlit as st
+from prophet import Prophet
+
+@st.cache_data
+def load_data(code):
+    #reading the data
+    df = pd.read_csv("data/{}.csv".format(code))
+
+    #convert string to date
+    df['Daily Date']=pd.to_datetime(df["Daily Date"],dayfirst=True)
+    df['Daily Date'] = df["Daily Date"].reset_index(drop=True)
+
+    #sorting the dates from highest to lowest
+    df.sort_values(by='Daily Date')
+    return df
+
+#Plot Opening Price
+def plot_opening(df):
+    plot_data(df, "Opening Price (GH¢)", "Daily Date", "Opening Price (GH¢)")
+
+def plot_change(df):
+    plot_data(df, "Price Change (GH¢)", "Daily Date", "Price Change (GH¢)")
+
+#Generic Plot function
+def plot_data(df, title, xlabel, ylabel):
+    # Creating the figure
+    fig = go.Figure()
+
+    #Plotting the data
+    fig.add_trace(go.Scatter(x=df[xlabel],y=df[ylabel],name=title))
+
+    #Adding the xaxis slider
+    fig.layout.update(title_text="Time Series Data",xaxis_rangeslider_visible=True) # type: ignore
+
+    #Plotting the figure on Streamlit
+    st.plotly_chart(fig)
+
+# Training for prophet
+def train_prophet(df,period):
+
+    #
+    df = df.reset_index()
+    df = df.rename(columns={'Daily Date': 'ds', 'Opening Price (GH¢)': 'y'})
+    m = Prophet()
+    m.fit(df)
+    future = m.make_future_dataframe(periods=period)
+    forecast = m.predict(future)
+    return forecast, future
--- main.py
+++ main.py
@@ -16,3 +16,33 @@
 
 # n_years = st.slider("Years of prediction:", 1,2, 3)
 # period = n_years*365
+
+# Import packages and data
+# import streamlit as st
+
+from helper import*
+
+#Render page with Streamlit
+st.title("Stock Predicition on the GSE")
+
+stocks = ("AADS","ACCESS","AGA","ETI","MTNGH","SCB","TLW","TOTAL")
+selected_stocks = st.selectbox("select dataset for prediction", stocks)
+
+n_years = st.slider("Years of prediction:", 1,2, 3)
+period = n_years*365
+
+#Load data
+data_load_state = st.text("Loading data...")
+df = load_data(selected_stocks)
+data_load_state.text("Loading data...done!")
+
+#plot_opening(df)
+st.subheader('Raw Data')
+st.write(df.tail())
+plot_opening(df)
+
+#plot forecast
+st.subheader('Forecast')
+
+#get forecast
+forecast, future = train_prophet(df,period)
